04c55c958de5e72e461206d994e8369a64644573
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2022-06-22 09:50:42 -0700

    Use Git utilities to determine source files (#16)
    * Use Git utilities to determine source files
    
    * Improve error message to reflect new method
    
    * Align leading strip

--- tools/nfkc.py
+++ tools/nfkc.py
@@ -1,14 +1,18 @@
 import unicodedata
-import os
+import subprocess
 import sys
+
+files = subprocess.run(
+    ["git", "ls-files", "--recurse-submodules"], check=True, capture_output=True
+).stdout.splitlines()
 
 fix = "--fix" in [unicodedata.normalize("NFKC", i) for i in sys.argv]
 
-for README_file in [i for i in os.listdir() if i.startswith("README.")]:
-  contents = open(README_file).read()
-  normalized = unicodedata.normalize("NFKC", contents)
-  if contents != normalized:
-    if fix:
-      open(README_file, "w").write(normalized)
-    else:
-      raise SystemExit('Error: README.* files are not NFKC normalized.')
+for file in files:
+    contents = open(file).read()
+    normalized = unicodedata.normalize("NFKC", contents)
+    if contents != normalized:
+        if fix:
+            open(file, "w").write(normalized)
+        else:
+            raise SystemExit("Error: Source files are not NFKC normalized.")
